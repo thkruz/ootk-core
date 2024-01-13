06340e0cb45cceaaaa18ffaa3ec86c7e2a5c47f3
Theodore Kruczek <theodore.kruczek@gmail.com>
2024-01-13 00:12:29 -0500

    build: :heavy_minus_sign: replace rimraf with fs

--- scripts/cleanup.mjs
+++ scripts/cleanup.mjs
@@ -1,23 +1,15 @@
-/* eslint-disable no-console */
-import rimraf from 'rimraf';
+import { rmSync } from 'fs';
 
 console.log('Removing ./lib...');
 try {
-  rimraf.sync('./lib');
+  rmSync('./lib', { recursive: true });
 } catch (error) {
   // Intentionally left blank
 }
 
 console.log('Removing ./commonjs...');
 try {
-  rimraf.sync('./commonjs');
-} catch (error) {
-  // Intentionally left blank
-}
-
-console.log('Removing ./mjs...');
-try {
-  rimraf.sync('./mjs');
+  rmSync('./commonjs', { recursive: true });
 } catch (error) {
   // Intentionally left blank
 }
