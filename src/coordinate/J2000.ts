f77390ce1d6c5ee4ee3ce6344c85a6fba3329814
Theodore Kruczek <theodore.kruczek@gmail.com>
2024-01-14 16:48:03 -0500

    test: :white_check_mark: add tests for J2000

--- src/coordinate/J2000.ts
+++ src/coordinate/J2000.ts
@@ -5,9 +5,23 @@ import { ITRF } from './ITRF';
 import { StateVector } from './StateVector';
 import { TEME } from './TEME';
 
+/**
+ * Represents a position and velocity in the J2000 coordinate system. This is an
+ * Earth-centered inertial (ECI) coordinate system.
+ *
+ * Commonly used ECI frame is defined with the Earth's Mean Equator and Mean
+ * Equinox (MEME) at 12:00 Terrestrial Time on 1 January 2000. It can be
+ * referred to as J2K, J2000 or EME2000. The x-axis is aligned with the mean
+ * vernal equinox. The z-axis is aligned with the Earth's rotation axis (or
+ * equivalently, the celestial North Pole) as it was at that time. The y-axis is
+ * rotated by 90° East about the celestial equator.
+ *
+ * @see https://en.wikipedia.org/wiki/Earth-centered_inertial
+ */
 export class J2000 extends StateVector {
   /**
-   * Creates a J2000 instance from classical elements.
+   * Creates a J2000 coordinate from classical elements.
+   * @param elements The classical elements. @returns The J2000 coordinate.
    */
   static fromClassicalElements(elements: ClassicalElements): J2000 {
     const rv = elements.toPositionVelocity();
@@ -17,6 +31,7 @@ export class J2000 extends StateVector {
 
   /**
    * Gets the name of the coordinate system.
+   * @returns The name of the coordinate system.
    */
   get name(): string {
     return 'J2000';
@@ -24,6 +39,8 @@ export class J2000 extends StateVector {
 
   /**
    * Gets a value indicating whether the coordinate system is inertial.
+   * @returns A boolean value indicating whether the coordinate system is
+   * inertial.
    */
   get inertial(): boolean {
     return true;
@@ -34,6 +51,8 @@ export class J2000 extends StateVector {
    * Reference Frame (ITRF).
    *
    * This is an ECI to ECF transformation.
+   *
+   * @returns The ITRF coordinates.
    */
   toITRF(): ITRF {
     const p = Earth.precession(this.epoch);
@@ -49,6 +68,11 @@ export class J2000 extends StateVector {
     return new ITRF(this.epoch, rPEF, vPEF);
   }
 
+  /**
+   * Converts the J2000 coordinate to the TEME coordinate.
+   *
+   * @returns The TEME coordinate.
+   */
   toTEME(): TEME {
     const p = Earth.precession(this.epoch);
     const n = Earth.nutation(this.epoch);
