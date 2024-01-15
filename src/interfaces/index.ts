527d368f81ee010bc3202d167cf1ceed9550a33e
Theodore Kruczek <theodore.kruczek@gmail.com>
2024-01-15 13:33:15 -0500

    fix: :adhesive_bandage: remove old import

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -3,7 +3,6 @@ export type { GroundPositionParams } from './GroundPositionParams';
 export type { OptionsParams } from './OptionsParams';
 export type { SatelliteParams } from './SatelliteParams';
 export type { SensorParams } from './SensorParams';
-export type { RfSensorParams } from './RfSensorParams';
 export type { StarObjectParams } from './StarObjectParams';
 export type { ClassicalElementsParams } from './ClassicalElementsParams';
 export type { EquinoctialElementsParams } from './EquinoctialElementsParams';
