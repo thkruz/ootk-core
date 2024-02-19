0453c0a3b05cc6b1a012301fd909f8a31925e7ec
Theodore Kruczek <theodore.kruczek@gmail.com>
2024-02-19 03:30:21 -0500

    refactor: :recycle: allow null rcs and vmag

--- src/interfaces/SatelliteParams.ts
+++ src/interfaces/SatelliteParams.ts
@@ -6,11 +6,11 @@ import { EciVec3, SpaceObjectType, TleLine1, TleLine2 } from '../types/types';
 
 export interface SatelliteParams {
   name?: string;
-  rcs?: number;
+  rcs?: number | null;
   tle1: TleLine1;
   tle2: TleLine2;
   type?: SpaceObjectType;
-  vmag?: number;
+  vmag?: number | null;
   sccNum?: string;
   intlDes?: string;
   position?: EciVec3;
